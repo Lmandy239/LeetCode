c72aae54c90243f5edd805f908fe34443a519aa4
Mandy <Lmandy239@gmail.com>
2024-08-30 21:45:58 -0400

    max subarray

---
+++ maxSubArray.java
@@ -0,0 +1,14 @@
+class maxSubArray {
+    public int maxSubArray(int[] nums) {
+    int maxSoFar = Integer.MIN_VALUE, maxEndingHere = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            maxEndingHere += nums[i];
+
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+
+            maxEndingHere = Math.max(maxEndingHere, 0);
+        }
+        return maxSoFar;
+    }
+}
